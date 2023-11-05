49c508479106fc5104d4c5203fef1e3de8932681
Shiv Karn <99037990+Lifehacker000@users.noreply.github.com>
2023-11-05 21:18:15 +0530

    Update SignupPage.js

---
+++ src/components/SignupPage.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+class SignUpPage extends React.Component {
+  render() {
+    return (
+      <div className="box">
+        <form autoComplete="off">
+          <a href="index.html" style={{ textDecoration: 'none', fontSize: '20px' }}>Personal Finance Tracker</a>
+          <h3>Sign up</h3>
+          <div className="inputBox">
+            <input type="text" required="required" />
+            <span>Userame</span>
+            <i></i>
+          </div>
+          <div className="inputBox">
+            <input type="text" required="required" />
+            <span>Mobile no.</span>
+            <i></i>
+          </div>
+          <div className="inputBox">
+            <input type="email" required="required" />
+            <span>Email</span>
+            <i></i>
+          </div>
+          <div className="inputBox">
+            <input type="password" required="required" />
+            <span>Password</span>
+            <i></i>
+          </div>
+          <div className="inputBox">
+            <input type="password" required="required" />
+            <span>Confirm Password</span>
+            <i></i>
+          </div>
+          <div className="links">
+            <a href="login.html">Already registered? Sign in</a>
+          </div>
+          <input type="submit" value="Signup" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignUpPage;
