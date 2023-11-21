79b1bef70b90f1bb06df6a3f453b1c77c6fc3b2f
Piyush Katyal <109459034+piyushk77@users.noreply.github.com>
2023-11-22 04:06:42 +0530

    feat: connect signup and login to backend

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import HomePage from './components/HomePage';
 import LoginPage from './components/LoginPage';
 import SignupPage from './components/SignupPage';
-import SetExpense from './components/SetExpense';
-import ExpenseLog from './components/ExpenseLog';
-import SetIncome from './components/SetIncome';
-import IncomeLog from './components/IncomeLog';
-import Budget from './components/Budget';
+// import SetExpense from './components/SetExpense';
+// import ExpenseLog from './components/ExpenseLog';
+// import SetIncome from './components/SetIncome';
+// import IncomeLog from './components/IncomeLog';
+// import Budget from './components/Budget';
 
 
 function App() {
@@ -21,11 +21,11 @@ function App() {
           <Route path="/signup" component={SignupPage} />
 
           {/* Following components need to be created */}
-          <Route path="/set/expense" component={SetExpense} />
+          {/* <Route path="/set/expense" component={SetExpense} />
           <Route path="/expense/log" component={ExpenseLog} />
           <Route path="/set/income" component={SetIncome} />
           <Route path="/income/log" component={IncomeLog} />
-          <Route path="/budget" component={Budget} />
+          <Route path="/budget" component={Budget} /> */}
 
         </Switch>
       </div>
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,25 +1,97 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import axios from 'axios';
 import Header from './Header';
 import Footer from './Footer';
+import './LoginPage.css';
 
 function LoginPage() {
-    const headerLinks = [
-        { url: '/', text: 'Home' },
-        { url: '/login', text: 'Login' },
-        { url: '/about', text: 'About' },
-        { url: '/contact', text: 'Contact' }
-    ];
+  const history = useHistory(); // Use useHistory hook
 
-    return (
-        <div className="login-page">
+  const headerLinks = [
+    { url: '/', text: 'Home' },
+    { url: '/signup', text: 'Sign up' },
+    { url: '/about', text: 'About' },
+    { url: '/contact', text: 'Contact' }
+  ];
 
-            <Header links={headerLinks} />
+  // State to store form data
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
 
-            <h1>Login Page</h1>
+  // Function to handle form input changes
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
 
-            <Footer />
-        </div>
-    );
+   // Function to handle form submission
+   const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('http://localhost:3500/api/auth/login', formData);
+
+      if (response.status === 200) {
+        // Login successful
+        console.log('Login successful');
+
+        // Store the JWT token in local storage
+        localStorage.setItem('authToken', response.data.token);
+
+        // Redirect to the dashboard or another protected page
+        history.push('/dashboard');
+      } else {
+        // Login failed
+        console.error('Login failed');
+        // Handle the error, show a message to the user, etc.
+      }
+    } catch (error) {
+      console.error('Error during login', error);
+      // Handle the error, show a message to the user, etc.
+    }
+  };
+
+  return (
+    <div className="login-page">
+      <Header links={headerLinks} />
+
+      <div className="login-box">
+        <form className="login-form" autoComplete="off" onSubmit={handleLogin}>
+          <h3 className="login-main-head">Login</h3>
+          <div className="inputBox">
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleInputChange}
+              required="required"
+            />
+            <span>Email</span>
+            <i></i>
+          </div>
+          <div className="inputBox">
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleInputChange}
+              required="required"
+            />
+            <span>Password</span>
+            <i></i>
+          </div>
+          <div className="links">
+            <Link to="/signup">Not yet registered? Sign up</Link>
+          </div>
+          <input type="submit" value="Login" />
+        </form>
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default LoginPage;
